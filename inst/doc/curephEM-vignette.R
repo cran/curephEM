41aaeaa40dcb111e8b21b58338bf256126f67223
Jue (Marquis) Hou <hou00123@umn.edu>
2024-02-02 05:50:02 +0000

    version 0.3.0

--- inst/doc/curephEM-vignette.R
+++ inst/doc/curephEM-vignette.R
@@ -1,20 +1,18 @@
-## ---- results= 'hide'---------------------------------------------------------
+## ----results= 'hide'----------------------------------------------------------
 library(curephEM)
+devtools::load_all()
+set.seed(531)
 
 ## -----------------------------------------------------------------------------
 sim.cureph.data = cureph.simgen()
-attach(sim.cureph.data)
 
-## ---- results='markup'--------------------------------------------------------
+## ----results='markup'---------------------------------------------------------
 attr(sim.cureph.data, 'true.coef')
 
-## ---- results= 'hide'---------------------------------------------------------
-Surv.cure(time,time2,event,origin=0,end=20)
-
-## ---- results= 'hide',warning=F-----------------------------------------------
+## ----results= 'hide',warning=F------------------------------------------------
 fit=cureph(Surv.cure(time,time2,event,origin=0,end=20)~Z1+Z2+Z3+Z4,data=sim.cureph.data)
 
-## ---- results= 'hide',warning=F-----------------------------------------------
+## ----results= 'hide',warning=F------------------------------------------------
 fit2=cureph(Surv.cure(time,time2,event,origin=0,end=20)~Z1+Z2+Z3+Z4,
   formula2 = ~ Z1+Z2,data=sim.cureph.data)
 
@@ -24,10 +22,7 @@ summary(fit)
 ## -----------------------------------------------------------------------------
 mysurv = survpred(fit)
 
-## ---- fig.show='hold',fig.cap = "Left: Estimated marginal survival at mean-baselevel. Right: Baseline survival in cox part. "----
+## ----fig.show='hold',fig.cap = "Left: Estimated marginal survival at mean-baselevel. Right: Baseline survival in cox part. "----
 plot(mysurv)
 plot(mysurv, pooled = F)
-
-## -----------------------------------------------------------------------------
-detach(sim.cureph.data)
 
